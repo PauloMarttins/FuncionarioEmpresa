a356806d39da2c835abb8198e4673f9194a03e6d
Paulo Henrique <paulomarttinshcs@gmail.com>
2024-02-27 09:13:06 -0300

    Primeiro Post

---
+++ FuncionariosEmpresa/Entities/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncionariosEmpresa.Entities
+{
+    internal class Employee
+    {
+        //Delcara-se as propriedades (Name, Hours, ValuePerHour)
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        //Construtor vazio.
+        public Employee()
+        { 
+        }
+
+        //Construtor com argumentos.
+        //Declara-se as variaveis name, hours, valuePerHour como parametro deste construtor.
+        //Cada uma das propriedades ora declarados, recebem respectivamente as variaveis compativeis com suas funcionalidades.
+        //A ordem de cada variavel é extremamente importante.
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        //Para realizar a liberação da operação de apagamento utiliza-se o modificador "virtual"
+        //Dessa forma, A Plavra em amarelo que representa a declaração desse metodo poderá ser modificada em uma classe derivada.
+        //ou seja, A classe que receber esta classe como herança.
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
---
+++ FuncionariosEmpresa/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using FuncionariosEmpresa.Entities;
+using System.Globalization;
+
+namespace FuncionariosEmpresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para poder utilizar o List é obrigatorio o (using System.Collections.Generic;) no cabeçalho.
+            //E como (Employee) é de outra classe, também é obrigatório o (using FuncionariosEmpresa.Entities;) no cabeçalho.
+            //Declara-se e Instancia-se a variavel ListEmployee do tipo Employee, a qual alocará todas as informações do Array.
+            List<Employee> ListEmployee = new List<Employee>();
+
+            //O Console escreve o comando para o usuário informar a quantidade de funcionarios.
+            Console.Write("Enter the number of Employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Para int i igual a "posição", enquanto i for menor que (numero de funcionarios) faça o algoritimo depois retorna i++ (uma posição a mais) à cada rodada.
+            for (int i = 0; i < n; i++)
+            {
+                //Interpolação
+                Console.WriteLine($"Employee #{i + 1} data:");
+                Console.Write("Outsourced (y/n)?");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                //Condição: Se ch (yes/no) for igual a Y faça:
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+                    ListEmployee.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                //Se não, faça:
+                else
+                {
+                    ListEmployee.Add(new Employee(name, hours , valuePerHour));
+
+                }
+                Console.WriteLine();
+                Console.WriteLine("PAYMENTS: ");
+
+                //Para cada variavel emp do tipo Employee alocado no arrayList ListEmployee faça:
+                foreach (Employee emp in ListEmployee)
+                {
+                    Console.WriteLine(emp.Name + " - $ " +emp.Payment().ToString("C2", CultureInfo.CurrentCulture));
+                }
+            }
+        }
+    }
+}
+
+
